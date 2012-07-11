a585a4769001454c9dd59b7dff6406b9f8799911
Katharina Hayer <katharinaehayer@gmail.com>
2012-07-11 10:52:26 -0400

    updated

--- fast_script.rb
+++ fast_script.rb
@@ -31,16 +31,18 @@ File.open(file).each do |line|
   line = line.split("\t")
   info = line[sample_number].split(":")
 
+  valid = 1
+
   next if  !(info[0] =~ /1\/1/) && !(info[0] =~ /0\/1/) && !(info[0] =~ /0\/2/) && !(info[0] =~ /2\/2/)
-  next if info[2].to_i < 10
-  next if info[3].to_f < 30.0
+  valid = 0 if info[2].to_i < 10
+  valid = 0 if info[3].to_f < 30.0
 
   # Compare it to control
 
   info_control = line[control_number].split(":")
 
-  next if info_control[0] =~ /1\/1/ && !(info[0] =~ /2\/2/) && info[3].to_f >= 20.0
-  next if info_control[0] == info[0] &&  info[3].to_f >= 20.0
+  valid = 0 if info_control[0] =~ /1\/1/ && !(info[0] =~ /2\/2/) && info[3].to_f >= 20.0
+  valid = 0 if info_control[0] == info[0] &&  info[3].to_f >= 20.0
 
 
 
@@ -51,7 +53,7 @@ File.open(file).each do |line|
   num_all_reads = info[2].to_f
   num_mut_reads = num_all_reads if num_all_reads < num_mut_reads
   frequency = num_mut_reads / num_all_reads
-  puts "#{line[0]}\t#{line[1]}\t#{frequency}"
+  puts "#{line[0]}\t#{line[1]}\t#{num_mut_reads.to_i}\t#{num_all_reads.to_i}\t#{info[3]}\t#{valid}"
   #puts info.join(":")
   #puts info_control.join(":")
   #exit if frequency < 0.1
