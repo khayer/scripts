7c548f5d2be15f301a7a27d92cf286f2bdbf730b
Katharina Hayer <katharinaehayer@gmail.com>
2014-02-20 16:46:09 -0500

    hitting the pause button

--- generate_geneseq_from_blast_sam.rb
+++ generate_geneseq_from_blast_sam.rb
@@ -142,7 +142,7 @@ def process_reads(reads, current_range,contigs,outfile_handle,path_to_trinity)
     File.open(out_trinity).each do |line|
       line.chomp!
       fields = line.split(" ")
-      line = "#{line[0]}#{current_range.join(":")} #{line[1..-1].join(" ")}" if line =~ /^>/
+      line = "#{fields[0]}#{current_range.join(":")} #{fields[1..-1].join(" ")}" if line =~ /^>/
       outfile_handle.puts line
     end
     `rm high_quality.fasta`
